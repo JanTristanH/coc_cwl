a10b9aaa2f56944c50067d8e6b525bd0bea006a0
Tristan <jan_tristan.heisecke@nordakademie.de>
2021-04-28 11:06:21 +0200

    sample maria requests

---
+++ mariaDriver.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const mariadb = require('mariadb');
+const pool = mariadb.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port:process.env.DB_PORT,
+    connectionLimit: 5
+});
+
+async function asyncFunction() {
+    let conn;
+    try {
+
+        conn = await pool.getConnection();
+        const rows = await conn.query("SELECT 1 as val");
+        console.log(rows);
+        // rows: [ {val: 1}, meta: ... ]
+
+        //const res = await conn.query("INSERT INTO myTable value (?, ?)", [1, "mariadb"]);
+        // res: { affectedRows: 1, insertId: 1, warningStatus: 0 }
+
+    } catch (err) {
+        throw err;
+    } finally {
+        if (conn) conn.release(); //release to pool
+    }
+}
+
+asyncFunction();
